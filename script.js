b1ebdebfdb2ed3b29e88085da435e61099e3f089
Willdeveloppeur <wilfried.rolland.dev@gmail.com>
2019-09-19 21:06:40 +0200

    Commit with first translate on menu-left and menu-right

--- script.js
+++ script.js
@@ -13,10 +13,22 @@ function openMenu(){
     if (burgDisplay == 'none'){
         document.getElementById('menu-right').style.display='block';
         document.getElementById('menu-left').style.display='block';
-        
+        document.getElementById("menu-right").animate([
+            { transform: 'translateX(100%)' }, 
+            { transform: 'translateX(0%)' }
+          ], { 
+            duration: 0
+          });
+          document.getElementById("menu-left").animate([
+            { transform: 'translateX(100%)' }, 
+            { transform: 'translateX(0%)' }
+          ], { 
+            duration: 500
+          });
     }else{
         document.getElementById('menu-right').style.display='none';
         document.getElementById('menu-left').style.display='none';
+        
     }
 }
 
